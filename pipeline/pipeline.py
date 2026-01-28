433f5e6a14a407253940ea338c8e5bb778918706
badri203 <badri19n@gmail.com>
2026-01-28 23:10:10 +0000

    Save progress

---
+++ pipeline/pipeline.py
@@ -0,0 +1,4 @@
+import sys
+
+month = sys.argv[1]
+print("Month:", month)
--- test/script.py
+++ test/script.py
@@ -3,4 +3,14 @@ from pathlib import Path
 current_dir = Path.cwd()
 current_file = Path(__file__).name
 
-print(f"Current Directory: {current_dir}")
+print(f"Files in the {current_dir} are:")
+
+for filepath in current_dir.iterdir():
+    if filepath.name == current_file:
+        continue
+    
+    print(f" -  {filepath.name}")
+
+    if filepath.is_file():
+        content = filepath.read_text(encoding='utf-8').strip()
+        print(f" content: {content}")
